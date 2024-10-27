4f8e1e99c8a9b321fe61b84a825623dcd2b5651f
Mykola Pozniak <sheinnickolas@gmail.com>
2024-10-27 11:42:09 +0100

    lab 2 is done

---
+++ production/src/main/java/org/example/services/vehicle/VehicleService.java
@@ -0,0 +1,4 @@
+package org.example.services.vehicle;
+
+public class VehicleService {
+}
---
+++ production/src/main/java/org/example/services/vehicleType/VehicleTypeServiceImplementation.java
@@ -0,0 +1,4 @@
+package org.example.services.vehicleType;
+
+public class VehicleTypeServiceImplementation {
+}
