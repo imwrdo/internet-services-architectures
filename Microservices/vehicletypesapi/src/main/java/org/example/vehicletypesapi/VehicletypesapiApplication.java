a12131951c85dd5017e390debc1fb78cfa69a282
Mykola Pozniak <sheinnickolas@gmail.com>
2024-11-15 14:19:58 +0100

    create workspace

---
+++ Microservices/vehicletypesapi/src/main/java/org/example/vehicletypesapi/VehicletypesapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.vehicletypesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicletypesapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicletypesapiApplication.class, args);
+    }
+
+}
---
+++ Microservices/vehicletypesapi/src/test/java/org/example/vehicletypesapi/VehicletypesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vehicletypesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicletypesapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
